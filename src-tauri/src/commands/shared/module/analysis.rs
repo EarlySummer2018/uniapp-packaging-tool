482864514d133c06f82e2718a3fcbdccdc414ae8
earlySummer <early.summer.cx@gmail.com>
2026-06-10 16:34:14 +0800

    fix: 配置 PayPal 官方 Maven 仓库凭据

--- src-tauri/src/commands/shared/module/analysis.rs
+++ src-tauri/src/commands/shared/module/analysis.rs
@@ -20,7 +20,7 @@ const PAYPAL_MAVEN_REPOSITORY: &str = r#"maven {
     url "https://cardinalcommerceprod.jfrog.io/artifactory/android"
     credentials {
         username 'paypal_sgerritz'
-        password 'YOUR_JFROG_PASSWORD_HERE'
+        password 'AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ'
     }
 }"#;
 
